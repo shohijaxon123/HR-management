7342876507b298fa5efbe350663aa5737861844e
User <User@WIN-NBCV8MHK300>
2024-04-08 15:55:17 +0500

    Добавьте файлы проекта.

---
+++ HR  management/Headers.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+using namespace std;
+#include <string>
+
+class Employee
+{
+private:
+	string name;
+	string position;
+	int salary;
+
+
+};
+
+
+
+class AddEmployee
+{
+
+
+
+};
---
+++ HR  management/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+	int  choice;
+	do
+	{
+		cout << "1. To add new employee" << endl;
+		cout << "0. To exit" << endl;
+		cout << "Input your choice: ";
+		cin >> choice;
+		switch (choice)
+		{
+
+
+		default:
+			break;
+		}
+
+
+
+	} while (choice !=0);
+	
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
