d178b747cd01042d2dceca271ffdd2d1783a3503
Daniil Pulatov <daniilpulatov2005@gmail.com>
2024-04-08 21:28:53 +0500

    Add constructors

--- HR  management/Headers.h
+++ HR  management/Headers.h
@@ -1,14 +1,29 @@
 #pragma once
 #include <iostream>
-using namespace std;
 #include <string>
+
+using namespace std;
+
 
 class Employee
 {
 private:
 	string name;
 	string position;
-	int salary;
+	double salary;
+public:
+	// Default constructor
+	Employee(){
+		name = "Unknown";
+		position = "Unknown";
+		salary = 0.0;
+	}
+	// Parametrized constructor
+	Employee(string name, string position, double salary ){
+		this->name = name;
+		this->position = position;
+		this->salary = salary;
+	}
 
 
 };
